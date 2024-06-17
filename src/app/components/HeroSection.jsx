01a82de4d4f30bb088f99512b147302fcb4a8703
cataka <saibaimanengineer@gmail.com>
2024-06-17 19:32:19 +0300

    FEAT: user session check at home page

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,46 +1,84 @@
 import React from "react";
+import { createClient } from "../utils/supabase/server";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
-const HeroSection = () => {
-  function HeroSectionItem({ imageUrl, title, description }) {
-    return (
-      <div className="h-[30rem] md:h-[calc(100vh-106px)] flex flex-col bg-cover bg-center bg-no-repeat">
-        <div className="mt-auto w-2/3 md:max-w-lg ps-5 pb-5 md:ps-10 md:pb-10">
-          <span className="block text-white">{title}</span>
-          <span className="block text-white text-xl md:text-3xl">
-            {description}
-          </span>
-          <div className="mt-5">
-            <a
-              href="#"
-              className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl bg-white border border-transparent text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
-            >
-              Read Case Studies
-            </a>
+const HeroSection = async () => {
+  const supabase = createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    const {} = await supabase.auth.signOut();
+    redirect("/login");
+  };
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello!{" "}
+              {session ? (
+                <span className="text-medium">{session.user.email}</span>
+              ) : (
+                <span className="text-medium">
+                  <br />
+                  Welcome to our page!
+                </span>
+              )}
+            </span>
+            <br />
+          </h1>
+          <div className="flex flex-row justify-between">
+            <p className="text-slate-600 text-base sm:text-xl mb-4">
+              Welcome to our secret restaurant website. Feel free to check out
+              our Takoyaki menu and order! Sign up now to receive exclusive
+              discounts! Stay updated with our special events and never miss out
+              on great deals. If you haven&apos;t registered yet, don&apos;t
+              wait!
+            </p>
+            <div className="col-span-5 mt-5 mb-6 lg:mt-0">
+              <Image
+                src="/images/sakura_wood_fubuki_6765.png"
+                alt="hero image"
+                className=""
+                width={250}
+                height={250}
+              />
+            </div>
+          </div>
+          <div className="mb-5 flex flex-row">
+            {session ? (
+              <form action={signOut}>
+                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 font-semibold text-white">
+                  Logout
+                </button>
+              </form>
+            ) : (
+              <>
+                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 font-semibold text-white">
+                  <Link href="/login">Sign In</Link>
+                </button>
+                <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 font-semibold text-black mt-3">
+                  <Link href="/signup">
+                    <span className="block bg-pink-300 hover:bg-pink-200 rounded-full px-5 py-2">
+                      Sign Up
+                    </span>
+                  </Link>
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
-    );
-  }
-
-  function HeroSection() {
-    return (
-      <div className="relative overflow-hidden w-full h-[30rem] md:h-[calc(100vh-106px)] bg-gray-100 rounded-2xl dark:bg-neutral-800">
-        {/* Item 1 */}
-        <HeroSectionItem
-          imageUrl="https://images.unsplash.com/photo-1615615228002-890bb61cac6e?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          title="Nike React"
-          description="Rewriting sport's playbook for billions of athletes"
-        />
-
-        {/* Item 2 */}
-        <HeroSectionItem
-          imageUrl="https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          title="CoolApps"
-          description="Your description for CoolApps"
-        />
-      </div>
-    );
-  }
+    </section>
+  );
 };
 
 export default HeroSection;
--- src/app/components/MenuSection.jsx
+++ src/app/components/MenuSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import ProductCard from "./ProductCard";
+import { createClient } from "../utils/supabase/server";
+import { redirect } from "next/navigation";
 
 const MenuData = [
   {
@@ -8,7 +10,7 @@ const MenuData = [
     name: "Takoyaki",
     description:
       "The skin is crispy, the inside is soft, and the octopus is plump. Signature takoyaki is absolutely delicious!!",
-    price: "3.44",
+    price: "6.5",
     image: "/images/product/1takoyaki.png",
   },
   {
@@ -16,7 +18,7 @@ const MenuData = [
     name: "Teritama",
     description:
       "The piping hot takoyaki is topped with a generous amount of our special teriyaki sauce and fresh egg salad made in-house. A rich and tasty takoyaki that is very satisfying to eat.",
-    price: "4.04",
+    price: "7.0",
     image: "/images/product/2teritama.png",
   },
   {
@@ -24,7 +26,7 @@ const MenuData = [
     name: "Negidako",
     description:
       "This refreshing Japanese-style takoyaki is made by topping hot takoyaki with fresh, crunchy spring onions and dipping it in the special tempura sauce that comes with it.",
-    price: "4.04",
+    price: "7.0",
     image: "/images/product/3negidako.png",
   },
   {
@@ -32,27 +34,33 @@ const MenuData = [
     name: "Cheese Mentai",
     description:
       "Hot takoyaki topped with a generous amount of special mentaiko mayonnaise made with Hakata mentaiko and two types of cheese (Parmesan and mozzarella). These takoyaki are popular for their rich flavor.",
-    price: "4.04",
+    price: "7.0",
     image: "/images/product/4cheesementai.png",
   },
   {
     id: 5,
     name: "Family Pack (16 pieces)",
     description:
-      "Absolutely delicious!! Get even more value for your money on takoyaki!",
-    price: "6.83",
+      "Absolutely delicious!! Get even more value for your money on takoyaki! Enjoy them with your friends or family :)",
+    price: "12.5",
     image: "/images/product/5_16takoyakiset.png",
   },
   {
     id: 6,
     name: "Danran Pack (24 pieces)",
-    description: "Absolutely delicious!! Let's all enjoy takoyaki!",
-    price: "9.97",
+    description:
+      "Absolutely delicious!! Let's all enjoy takoyaki! Enjoy them in a party or special events with your family or friends!",
+    price: "18.5",
     image: "/images/product/6_24takoyakiset.png",
   },
 ];
 
-const MenuSection = () => {
+const MenuSection = async () => {
+  const supabase = createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <section className="py-10" id="menu">
       <div className="flex flex-wrap justify-between py-4 bg-purple-200 rounded-lg">
@@ -78,15 +86,20 @@ const MenuSection = () => {
       </div>
 
       <div className="grid md:grid-cols-3 gap-8 md:gap-12 py-5">
-        {MenuData.map((product) => (
-          <ProductCard
-            key={product.id}
-            name={product.name}
-            description={product.description}
-            price={product.price}
-            image={product.image}
-          />
-        ))}
+        {MenuData.map((product) => {
+          const discountedPrice = session
+            ? `${(product.price * 0.8).toFixed(2)} (Discounted!)`
+            : product.price;
+          return (
+            <ProductCard
+              key={product.id}
+              name={product.name}
+              description={product.description}
+              price={discountedPrice}
+              image={product.image}
+            />
+          );
+        })}
       </div>
     </section>
   );
