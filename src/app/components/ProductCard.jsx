a4e2e8a9982cd0a400ddbb030878992bba75fc5b
cataka <saibaimanengineer@gmail.com>
2024-06-17 16:50:41 +0300

    FIX: added colors to the product's text to show

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -9,7 +9,7 @@ const ProductCard = ({ name, description, price, image }) => {
         <Image src={image} alt={name} layout="fill" objectFit="contain" />
       </div>
       <div className="p-4 flex flex-col flex-grow">
-        <h5 className="text-xl font-bold mb-2">{name}</h5>
+        <h5 className="text-xl text-black font-bold mb-2">{name}</h5>
         <p className="text-purple-500 font-bold">€{price}</p>
         <p className="text-gray-700 mb-4">{description}</p>
         <button className="mt-auto self-start px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition-colors">
